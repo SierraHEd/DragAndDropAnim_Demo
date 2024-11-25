dbccddc53183a026a8c9509c17b97e8515428583
SierraHEd <sierraedwards123@yahoo.com>
2024-11-25 15:00:42 -0500

    Complete TODO 6, 7, 8, and 9

--- app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
+++ app/src/main/java/edu/farmingdale/draganddropanim_demo/DragAndDropBoxes.kt
@@ -5,6 +5,12 @@ package edu.farmingdale.draganddropanim_demo
 import android.content.ClipData
 import android.content.ClipDescription
 import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.animateIntOffsetAsState
+import androidx.compose.animation.core.repeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.scaleIn
@@ -14,18 +20,34 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.draganddrop.dragAndDropSource
 import androidx.compose.foundation.draganddrop.dragAndDropTarget
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.KeyboardArrowDown
+import androidx.compose.material.icons.filled.KeyboardArrowLeft
+import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.KeyboardArrowUp
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -34,26 +56,30 @@ import androidx.compose.ui.draganddrop.DragAndDropEvent
 import androidx.compose.ui.draganddrop.DragAndDropTarget
 import androidx.compose.ui.draganddrop.DragAndDropTransferData
 import androidx.compose.ui.draganddrop.mimeTypes
-import androidx.compose.foundation.Canvas
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.ArrowForward
-import androidx.compose.material.icons.filled.Face
-import androidx.compose.material3.Icon
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+
+//private val rotation = FloatPropKey()
 
 
 @Composable
 fun DragAndDropBoxes(modifier: Modifier = Modifier) {
+    var isPlaying by remember { mutableStateOf(true) }
+    var lastDropPosition by remember { mutableStateOf(Offset(130f, 300f)) }
+    var animationDirection by remember { mutableStateOf("none") }
     Column(modifier = Modifier.fillMaxSize()) {
 
         Row(
             modifier = modifier
-                .fillMaxWidth().weight(0.2f)
+                .fillMaxWidth()
+                .weight(0.2f)
         ) {
             val boxCount = 4
             var dragBoxIndex by remember {
@@ -67,6 +93,21 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                         .fillMaxHeight()
                         .padding(10.dp)
                         .border(1.dp, Color.Black)
+                        .pointerInput(Unit) { // Track pointer position
+                            detectDragGestures { change, _ ->
+                                change.consume()
+                                //Moves in indicated direction
+                                val currentPosition = change.position
+                                animationDirection = when {
+                                    currentPosition.y < lastDropPosition.y -> "up"
+                                    currentPosition.y > lastDropPosition.y -> "down"
+                                    currentPosition.x < lastDropPosition.x -> "left"
+                                    currentPosition.x > lastDropPosition.x -> "right"
+                                    else -> "none"
+                                }
+                                lastDropPosition = currentPosition
+                            }
+                        }
                         .dragAndDropTarget(
                             shouldStartDragAndDrop = { event ->
                                 event
@@ -76,8 +117,7 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                             target = remember {
                                 object : DragAndDropTarget {
                                     override fun onDrop(event: DragAndDropEvent): Boolean {
-
-                                        dragBoxIndex = index
+                                        isPlaying = !isPlaying
                                         return true
                                     }
                                 }
@@ -90,7 +130,17 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                         enter = scaleIn() + fadeIn(),
                         exit = scaleOut() + fadeOut()
                     ) {
-                        Icon(imageVector = Icons.Default.ArrowForward, contentDescription = "",
+                        //Change Icon to indicate direction change
+                        val arrowIcon = when (animationDirection) {
+                            "up" -> Icons.Default.KeyboardArrowUp
+                            "down" -> Icons.Default.KeyboardArrowDown
+                            "left" -> Icons.Default.KeyboardArrowLeft
+                            "right" -> Icons.Default.KeyboardArrowRight
+                            else -> Icons.Default.ArrowForward
+                        }
+                        Icon(imageVector = arrowIcon,
+                            //Updates based on direction
+                            contentDescription = "Arrow Direction",
                             modifier = Modifier
                                 .fillMaxSize()
                                 .dragAndDropSource {
@@ -111,18 +161,56 @@ fun DragAndDropBoxes(modifier: Modifier = Modifier) {
                     }
                 }
             }
+            //Reset Button that sets Face back to Center
+            Button(
+                onClick = {
+                    isPlaying = true
+                    animationDirection = "none"
+                    lastDropPosition = Offset(130f, 300f)
+                },
+                modifier = Modifier
+                    .padding(10.dp)
+            ) {
+                Text("Reset")
+            }
         }
 
+//Offsets based on Direction
+        val pOffset by animateIntOffsetAsState(
+            targetValue = when (animationDirection) {
+                "up" -> IntOffset(130, 100)
+                "down" -> IntOffset(130, 500)
+                "left" -> IntOffset(50, 300)
+                "right" -> IntOffset(200, 300)
+                else -> IntOffset(130, 300)
+            },
+            animationSpec = tween(3000, easing = LinearEasing)
+        )
 
-        Canvas(
+        val rtatView by animateFloatAsState(
+            targetValue = if (isPlaying) 360f else 0.0f,
+            // Configure the animation duration and easing.
+            animationSpec = repeatable(
+                iterations = if (isPlaying) 10 else 1,
+                tween(durationMillis = 3000, easing = LinearEasing),
+                repeatMode = RepeatMode.Restart
+            )
+        )
+        Box(
             modifier = Modifier
                 .fillMaxWidth()
                 .weight(0.8f)
                 .background(Color.Red)
-
         ) {
-                drawRect(color = Color.Green, size = Size(100f, 150f), topLeft = Offset(100f, 100f))
-       }
+            Icon(
+                imageVector = Icons.Default.Face,
+                contentDescription = "Face",
+                modifier = Modifier
+                    .padding(10.dp)
+                    .offset(pOffset.x.dp, pOffset.y.dp)
+                    .rotate(rtatView)
+            )
+        }
     }
 }
 
